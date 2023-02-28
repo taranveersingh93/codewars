3225e6e3363706643441e18c97a41634662c44c0
Taranveer Singh <taranveersingh93@gmail.com>
2023-02-28 13:31:50 -0500

    Solve - are you playing banjo?

---
+++ banjo.js
@@ -0,0 +1,18 @@
+/* ARE YOU PLAYING BANJO?
+https://www.codewars.com/kata/53af2b8861023f1d88000832
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.
+*/ 
+function areYouPlayingBanjo(name) {
+    if(name.charAt(0)==="r" || name.charAt(0)==="R") {
+        return `${name} plays banjo`;
+    } else {
+        return `${name} does not play banjo`;
+    }
+  }
