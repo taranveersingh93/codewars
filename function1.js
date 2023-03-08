15c1d3aabf9c4a5abd7ba14be3f9fe100dd869bf
Taranveer Singh <taranveersingh93@gmail.com>
2023-03-07 23:51:07 -0500

    Solve - function 1 - hello world

---
+++ function1.js
@@ -0,0 +1,10 @@
+/* Function 1 - hello world
+https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+Make a simple function called greet that returns the most-famous "hello world!".
+*/
+
+
+
+function greet() {
+    return "hello world!";
+  }
