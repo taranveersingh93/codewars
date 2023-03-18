ad1b9ae80e100779c2c6f53c3f1c110338a9406d
Taranveer Singh <taranveersingh93@gmail.com>
2023-03-18 01:28:55 -0400

    Edit - cat years, dog years

--- catDogYears.js
+++ catDogYears.js
@@ -24,7 +24,7 @@ Dog Years
 */
 var humanYearsCatYearsDogYears = function(humanYears) {
     if(humanYears>2) {
-        return [humanYears, 24 + 4*humanYears, 24 + 5*humanYears];
+        return [humanYears, 24 + 4*(humanYears-2), 24 + 5*(humanYears-2)];
     } else if (humanYears>1) {
         return [humanYears, 24, 24];
     } else {
